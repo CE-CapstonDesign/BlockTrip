6a04d0afca2f76115a227064b6e6bbcd847ded92
baegyeong <beth0929@naver.com>
2023-12-18 22:37:26 +0900

    feat: 갯수 측정에 사용하는 Counter 컴포넌트 구현

---
+++ BlockTrip_FE/src/components/common/Counter.jsx
@@ -0,0 +1,43 @@
+const Counter = ({
+  section,
+  description,
+  min,
+  max,
+  number,
+  onNumberChange,
+}) => {
+  const BUTTON = `rounded-full border-2 border-green text-green w-5 h-5 flex items-center justify-center hover:text-white hover:border-green hover:bg-green`;
+
+  const handleOnMinus = () => {
+    if (number > min) {
+      onNumberChange(number - 1);
+    }
+  };
+
+  const handleOnPlus = () => {
+    if (number < max) {
+      onNumberChange(number + 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center mb-10 space-x-8">
+      <div className="flex flex-col">
+        <p>{section}</p>
+        <p className="text-gray text-sm">{description}</p>
+      </div>
+
+      <div className="text-xl flex items-center space-x-4">
+        <button type="button" onClick={handleOnMinus} className={`${BUTTON}`}>
+          -
+        </button>
+        <span>{number}</span>
+        <button onClick={handleOnPlus} className={`${BUTTON}`}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
