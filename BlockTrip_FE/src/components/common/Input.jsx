d77eba3509c732e2e1171b1205d84b2f4e2be429
baegyeong <beth0929@naver.com>
2023-12-21 03:57:46 +0900

    style: 글씨 크기 조정

--- BlockTrip_FE/src/components/common/Input.jsx
+++ BlockTrip_FE/src/components/common/Input.jsx
@@ -1,7 +1,7 @@
 const Input = ({ className, register, ...inputProps }) => {
   return (
     <input
-      className={`border-b-2 border-gray ${className}`}
+      className={`border-b-2 border-gray text-lg ${className}`}
       {...register}
       {...inputProps}
     />
