477be77dd90bb7517844a3b32f537b44833466f0
baegyeong <beth0929@naver.com>
2023-12-21 03:57:12 +0900

    fix: counter 컴포넌트 input 태그 추가

--- BlockTrip_FE/src/components/common/Counter.jsx
+++ BlockTrip_FE/src/components/common/Counter.jsx
@@ -1,37 +1,40 @@
 const Counter = ({
   section,
   description,
+  name,
   min,
   max,
-  number,
+  value,
   onNumberChange,
 }) => {
   const BUTTON = `rounded-full border-2 border-green text-green w-5 h-5 flex items-center justify-center hover:text-white hover:border-green hover:bg-green`;
 
   const handleOnMinus = () => {
-    if (number > min) {
-      onNumberChange(number - 1);
+    if (value > min) {
+      onNumberChange(value - 1);
     }
   };
 
   const handleOnPlus = () => {
-    if (number < max) {
-      onNumberChange(number + 1);
+    if (value < max) {
+      onNumberChange(value + 1);
     }
   };
 
   return (
     <div className="flex items-center mb-10 space-x-8">
-      <div className="flex flex-col">
-        <p>{section}</p>
-        <p className="text-gray text-sm">{description}</p>
-      </div>
+      {section && (
+        <div className="flex flex-col">
+          <p>{section}</p>
+          <p className="text-gray text-sm">{description}</p>
+        </div>
+      )}
 
       <div className="text-xl flex items-center space-x-4">
         <button type="button" onClick={handleOnMinus} className={`${BUTTON}`}>
           -
         </button>
-        <span>{number}</span>
+        <input type="text" name={name} readOnly className="w-3" value={value} />
         <button onClick={handleOnPlus} className={`${BUTTON}`}>
           +
         </button>
