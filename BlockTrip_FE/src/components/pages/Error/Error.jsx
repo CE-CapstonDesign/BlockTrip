56e827ffe6d7fc14f94e5c8e825504c06011fd92
baegyeong <beth0929@naver.com>
2024-06-06 17:08:16 +0900

    feat: 에러 발생 시 에러 컴포넌트로 리다이렉트

--- BlockTrip_FE/src/components/pages/Error/Error.jsx
+++ BlockTrip_FE/src/components/pages/Error/Error.jsx
@@ -1,19 +1,21 @@
 import { Header } from "@/components/features/ui";
-import { useNavigate } from "react-router-dom";
 
 export const Error = ({ error }) => {
-  const navigate = useNavigate();
-
   return (
     <>
       <Header />
       <section className="flex flex-col justify-center items-center w-[100vw] h-[80vh]">
         <h1 className="text-5xl mb-6">ERROR</h1>
-        <p className="text-xl">에러가 발생했습니다. 관리자에게 문의하세요.</p>
-        <p className="text-sm">에러내용: {error}</p>
+        <p className="text-xl mb-8">
+          에러가 발생했습니다. 관리자에게 문의하세요.
+        </p>
+        <p className="text-lg">
+          에러내용: {error.message}
+          <br /> {error.response.data.error.message}
+        </p>
         <p
-          className="text-green text-xl cursor-pointer"
-          onClick={() => navigate("/")}
+          className="text-green text-xl cursor-pointer mt-10"
+          onClick={() => window.location.reload()}
         >
           홈으로 가기
         </p>
--- BlockTrip_FE/src/services/travel.js
+++ BlockTrip_FE/src/services/travel.js
@@ -1,4 +1,4 @@
-import instance from "./index";
+import { instance } from "./index";
 
 export const travelPlan = (data) => {
   return instance.post("/travel/plan", data);
